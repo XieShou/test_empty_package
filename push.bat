2968be36254b88d824ef3aa41de1ed44c628f3f0
XieShou <15029285063@163.com>
2022-05-09 00:31:06 +0800

    0.3.0

--- push.bat
+++ push.bat
@@ -1,14 +1,14 @@
 ::设置模块名字
 SET ToolName=upm-com.lonestar.empty
 ::设置模块版本
-SET Tag=0.2.0
+SET Tag=0.3.0
 ::设置模块源路径
-SET ToolAssetPath=Packages/com.lonestar.core
+SET ToolAssetPath=Packages/com.lonestar.empty
 
 ::此命令会创建一个ToolName的分支，并同步ToolAssetPath下的内容
 git subtree split --prefix=%ToolAssetPath% --branch %ToolName%
 :: 在ToolName分支设置标签ToolVersion节点
-git tag %ToolVersion% %ToolName% 
+git tag %Tag% %ToolName%
 
 :: 推送到远端
 git push origin %ToolName% --tags
