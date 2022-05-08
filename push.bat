cacd309cca83ec9b0b2dcd7223883be3b7248914
XieShou <15029285063@163.com>
2022-05-09 01:11:27 +0800

    0.7.0

--- push.bat
+++ push.bat
@@ -1,17 +1,17 @@
 ::设置模块名字
 SET ToolName=com.lonestar.empty
 ::设置模块版本
-SET Tag=0.5.0
+SET Tag=0.7.0
 
 ::设置模块源路径
 SET ToolAssetPath=Packages/com.lonestar.empty
 
 ::此命令会创建一个ToolName的分支，并同步ToolAssetPath下的内容
-git subtree split --prefix=%ToolAssetPath% --branch %BranchName%
+git subtree split --prefix=%ToolAssetPath% --branch %ToolName%
 :: 在ToolName分支设置标签ToolVersion节点
-git tag %Tag% %BranchName%
+git tag %Tag% %ToolName%
 
 :: 推送到远端
-git push origin %BranchName% --tags
-git push origin %BranchName%
+git push origin %ToolName% --tags
+git push origin %ToolName%
 pause
